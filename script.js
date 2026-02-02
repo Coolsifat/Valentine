8da226a24beb57fbe11ed18976ab68b65d3381c9
Coolsifat <sifatnoor10@gmail.com>
2026-02-03 02:45:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ envelope.addEventListener("click", () => {
 // Logic to move the NO btn
 
 noBtn.addEventListener("mouseover", () => {
-    const min = 50;
-    const max = 50;
+    const min = 100;
+    const max = 100;
 
     const distance = Math.random() * (max - min) + min;
     const angle = Math.random() * Math.PI * 2;
